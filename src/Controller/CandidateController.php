22dc1f28c86010971917d49aedd867f68a73eebb
elena100880 <100880kr@gmail.com>
2021-06-13 15:30:28 +0200

    sorting by ALL fields

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Validator\Exception\OutOfBoundsException;
-//use Symfony\Component\Validator\Exception\OutOfRangeException;
+
 
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,63 +28,62 @@ class CandidateController extends AbstractController
          * example json data from Request (some script gives us below data):
          * 
          * {    
-         *      "phrase": "php symfony",  //asume, that this record must be required and its length < 2000 characters
+         *      "phrase": "php symfony",  // length < 2000 characters
          * 
          *      "date1": "1990-08-10",   //or 0 if not chosen
          *      "date2" : 0,
          * 
-         *      "sorting": {         // I asume that sorting is made by only one field at once, 1 - ASC, 2 - DESC, 0 - without sorting by 
+         *      "sorting": {         //  1 - ASC, 2 - DESC, 0 - without sorting by this field; Only the last field with 1or2 values defines the order;
+         *                          //I asume that we can order by 3 below fields.
          *          "first_name": 2,  
-         *          "last_name": 0
+         *          "last_name": 0,
+         *          'tag': 0
          *       },   
-         * 
-         *      //flags - show or not tag and notes:  //asume that they are required, 1 -  show, 0 - not show
+         *        
+         *      //show or not tag and notes: 1 -  show, 0 - not show
          *      "tag": 1,  
          *      "notes": 0   
-         *      
          * }
          */
 
         $json = file_get_contents('php://input');
 
-//VALIDATION - valid json string, valid "phrase" (<2000 ), other records validation
+// some VALIDATION - valid json string, valid "phrase" (<2000 ), other records validation
         //I asume, that processing the below Exceptions are at some SCRIPT's side, which sends Request and gets Response from this endpoint
         
-        function check_json($json)
+        function valid_json($string)
         {
-            //some code for checking if $json is a valid json data
-            return true;
+            json_decode($string);
+            if (json_last_error() === 0) return true;
+            else throw new \JsonException ('Invalid json');
         }
-        if (!check_json($json)) throw new Exception ('Invalid json');
 
-
-        $data = json_decode($json, $associative=true); 
-        
-        if (!isset($data['phrase'])) throw new OutOfBoundsException ("No valid record of PHRASE");
-        if (strlen($data['phrase'])> 2000 ) throw new LengthException ("Invalid length of PHRASE");
-        else $phrase = $data['phrase'];
-        
-        function valid_date($date) {
+        function valid_date($date) {   //based on this source:https://stackoverflow.com/questions/19271381/correctly-determine-if-date-string-is-a-valid-date-in-that-format
             
             $dateAsObject = \DateTime::createFromFormat('Y-m-d', $date);
-            $ttt =  $dateAsObject->format('Y-m-d');
             if ( ($dateAsObject) and ($dateAsObject->format('Y-m-d') === $date) or $date == 0) return true;
-            else return false;
-        }
-        $r=valid_date('1-02-15' );
-
-        if (!isset($data['date1']) or  !isset($data['date2']) ) throw new OutOfBoundsException ("No valid key for date");
-        if ( valid_date($data['date1']) and valid_date($data['date2']) )
-        {
-            $date1 = $data['date1']; 
-            $date2 = $data['date2'];
+            else throw new OutOfBoundsException ("Invalid date value");
         }
 
-        if (!isset($data['sorting']['first_name']) or !isset($data['sorting']['last_name'] ) ) throw new OutOfBoundsException ("No valid key for FIRST/LAST_NAME");
-        if ( !in_array($data['sorting']['first_name'], [0,1,2]) or !in_array($data['sorting']['last_name'], [0,1,2] ) ) throw new RangeException ("Invalid sorting values");
+
+        if (valid_json($json)) $data = json_decode($json, $associative=true); ;
+       
+
+        if (!isset($data['phrase'])) throw new OutOfBoundsException ("No valid record for PHRASE");
+        if (strlen($data['phrase'])> 2000 ) throw new LengthException ("Invalid length of PHRASE");
+        else $phrase = $data['phrase'];
+     
         
+        if (!isset($data['date1']) or  !isset($data['date2']) ) throw new OutOfBoundsException ("No valid key for date");
+        if ( valid_date($data['date1']) and valid_date($data['date2']) ) {$date1 = $data['date1']; $date2 = $data['date2'];}
+        
+
+    /*    if (!isset($data['sorting']['first_name']) or !isset($data['sorting']['last_name']) or !isset($data['sorting']['tag'] )) throw new OutOfBoundsException ("No valid key for sorting field");
+        if ( !in_array($data['sorting']['first_name'], [0,1,2]) or !in_array($data['sorting']['last_name'], [0,1,2] ) or !in_array($data['sorting']['tag'], [0,1,2] ) ) throw new \RangeException ("Invalid sorting values");*/
+        
+
         if (!isset($data['tag']) or  !isset($data['notes']) ) throw new OutOfBoundsException ("No record of TAG/NOTES");
-        if ( !in_array($data['tag'], [0,1]) or !in_array($data['notes'], [0,1] ) ) throw new RangeException ("Invalid tag/notes values");
+        if ( !in_array($data['tag'], [0,1]) or !in_array($data['notes'], [0,1] ) ) throw new \RangeException ("Invalid tag/notes values");
                      
 
 //ElasticSearch for Candidates:
@@ -103,7 +102,7 @@ class CandidateController extends AbstractController
 
                 */
                 
-                $arrayOfCandidatesId = [1, 2, 3, 4, 5, 6];
+                $arrayOfCandidatesId = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12];
                 return $arrayOfCandidatesId;
         }
         $arrayOfCandidatesId = candidate_elastic_search($phrase, $date1, $date2);
@@ -115,13 +114,15 @@ class CandidateController extends AbstractController
                                         ->from('App\Entity\Candidate', 'c')
                                         ->setParameter('ids', $arrayOfCandidatesId)
                                         ->where('c.id in (:ids)');
-        if ($data['sorting']['first_name'] == 1) $queryBuilder= $queryBuilder->orderBy('c.first_name', 'ASC');
-        if ($data['sorting']['first_name'] == 2) $queryBuilder= $queryBuilder->orderBy('c.first_name', 'DESC');
-                
-        if ($data['sorting']['last_name'] == 1) $queryBuilder= $queryBuilder->orderBy('c.last_name', 'ASC');
-        if ($data['sorting']['last_name'] == 2) $queryBuilder= $queryBuilder->orderBy('c.last_name', 'DESC');
-         
-        else $queryBuilder= $queryBuilder->orderBy('c.id', 'ASC'); //on default - sorting by id
+        if ( $data['sorting'][1][key($data['sorting'][1])] == 1 ) $queryBuilder = $queryBuilder->orderBy('c.'.key($data['sorting'][1]), 'ASC');
+        if ( $data['sorting'][1][key($data['sorting'][1])] == 2) $queryBuilder = $queryBuilder->orderBy('c.'.key($data['sorting'][1]), 'DESC');
+
+        if ( $data['sorting'][2][key($data['sorting'][2])] == 1) $queryBuilder = $queryBuilder->addOrderBy('c.'.key($data['sorting'][2]), 'ASC');
+        if ( $data['sorting'][2][key($data['sorting'][2])] == 2) $queryBuilder = $queryBuilder->addOrderBy('c.'.key($data['sorting'][2]), 'DESC');
+        
+        if ( $data['sorting'][3][key($data['sorting'][3])] == 1) $queryBuilder = $queryBuilder->addOrderBy('c.'.key($data['sorting'][3]), 'ASC');
+        if ( $data['sorting'][3][key($data['sorting'][3])] == 2) $queryBuilder = $queryBuilder->addOrderBy('c.'.key($data['sorting'][3]), 'DESC');
+
          
         $candidates = $queryBuilder->getQuery()->getResult();
 
